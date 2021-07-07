7071ff576dc57c787ca6ac8e0ccf10b6a61406b1
Varun Kumar <varun-kumar@live.com>
2021-07-07 11:10:46 +0530

    article model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    IDEOLOGY = [
+        ('LW', 'Left Wing'),
+        ('RW', 'Right Wing')
+    ]
+    headline = models.CharField(max_length=255)
+    url = models.URLField(unique=True, max_length=2000)
+    ideology = models.CharField(choices=IDEOLOGY, max_length=10)
+    image_url = models.TextField()
+    published_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
